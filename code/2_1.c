8f4f61057244b84e9337a94c1ee52d9ad9e28a0a
mental1104 <mental1104@gmail.com>
2021-03-25 17:21:32 +0800

    Exercise 2-1

---
+++ code/2_1.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main(){
+    printf("Blue Espeon\n");
+
+    printf("Blue\n");
+    printf("Espeon\n");
+
+    printf("Blue ");
+    printf("Espeon\n");
+
+    return 0;
+}
