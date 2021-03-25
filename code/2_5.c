baa0c386dc2bff90be809b89abc4fdd9673d6850
mental1104 <mental1104@gmail.com>
2021-03-25 17:36:06 +0800

    Exercise 2_5

---
+++ code/2_5.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+void br(){
+    printf("Brazil, Russia");
+}
+
+void ic(){
+    printf("India, China");
+}
+
+int main(){
+    br();
+    printf(", ");
+    ic();
+    printf("\n");
+    ic();
+    printf(",\n");
+    br();
+    printf("\n");
+    return 0;
+}
