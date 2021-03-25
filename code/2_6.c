467a19aeb25c09d3598f298f4e5e8e85d5cf68c5
mental1104 <mental1104@gmail.com>
2021-03-25 17:39:12 +0800

    Exercise 2_6

---
+++ code/2_6.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+int main(){
+    int toes = 10;
+    printf("toes = %d\n", toes);
+    printf("toes * 2 = %d\n", toes * 2);
+    printf("The square of toes = %d\n", toes * toes);
+    return 0;
+}
